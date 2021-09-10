ee2e4e133534a1bc9e7f53ef75fbb51d6551085b
AKirosan <kirilenkoandr@yandex.ru>
2021-09-11 02:05:45 +0400

    next edit

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -57,11 +57,11 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',  # os.environ.get('DB_ENGINE'),
-        'NAME': 'postgres',  # os.environ.get('DB_NAME'),
-        'USER': 'postgres',  # os.environ.get('POSTGRES_USER'),
-        'PASSWORD': 'andripups1',  # os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',  # os.environ.get('DB_HOST'),
-        'PORT': '5432',  # os.environ.get('DB_PORT'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
