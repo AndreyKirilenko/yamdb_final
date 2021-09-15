4d6aac6df9af683c370bee6aefd6a4220abcdcb5
AKirosan <kirilenkoandr@yandex.ru>
2021-09-15 17:13:30 +0400

    смена default secret-key на cHaNgE-Me_NOW

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'SecRetKeyeDitNoW!!!')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='cHaNgE-Me_NOW!!!')
 
 DEBUG = False
 
