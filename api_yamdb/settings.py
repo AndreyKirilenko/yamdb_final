d088b3ad08c8e4e230fee9750d7265447e7ae126
AKirosan <kirilenkoandr@yandex.ru>
2021-09-15 22:26:50 +0400

    добавил default в secret_key

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get('SECRET_KEY')  #, default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='cHaNgE-Me_NOW!!!')  # , default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
 DEBUG = True
 # ALLOWED_HOSTS = ['http://84.201.179.146', 'localhost', '127.0.0.1', 'web']
